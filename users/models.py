794e3e568d72734deec4d5bf4329980280c523a3
faithoroone <faithorone59@gmail.com>
2018-08-17 05:20:02 +0300

    add models.py file

---
+++ users/models.py
@@ -0,0 +1,10 @@
+class Users:
+    def __init__(self, first_name, last_name, username, email, password, confirm_password):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.username = username
+        self.email = email
+        self.password =password
+        self.confirm_password = confirm_password
+
+users = []
